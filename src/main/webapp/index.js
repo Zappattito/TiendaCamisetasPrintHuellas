cf91eb30c2f66b2628d5e8c25658c92f8b0d3d8d
Zappattito <algarve318@gmail.com>
2024-04-28 21:53:22 +0200

    modificar color de modelo

---
+++ src/main/webapp/index.js
@@ -0,0 +1,31 @@
+window.addEventListener("DOMContentLoaded", function(){
+
+    let modeloMujerBlanco = document.getElementById("modeloMujerBlanco");
+
+    function actualizarColor() {
+        // Obtener el elemento select
+        var colorList = document.getElementById("colorList");
+        // Obtener el valor seleccionado en el elemento select
+        var colorSeleccionado = colorList.value;
+        // Actualizar el valor del input type="color" con el color seleccionado
+        document.getElementById("colorPicker").value = colorSeleccionado;
+    }
+
+   
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
